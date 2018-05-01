b91b43098cfa1f9f2f97991cf5221f65c5573087
lidongliang <dongfang369>
2018-05-01 17:46:28 +0800

    Initial commit

---
+++ app/src/main/java/iteast/com/circleprogressview/CircleProgressView.java
@@ -0,0 +1,22 @@
+package iteast.com.circleprogressview;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CircleProgressView extends View {
+
+    public CircleProgressView(Context context) {
+        super(context);
+    }
+
+    public CircleProgressView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleProgressView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
